62bab5b669d6b162cf1a8bd9279a46ed8a78ca5c
mcalvot <Esma2019*>
2019-03-21 14:31:52 +0100

    first commit

---
+++ src/main/java/org/mcalvot/formacion/AccesingDataWithMySQL/AccesingDataWithMySqlApplication.java
@@ -0,0 +1,13 @@
+package org.mcalvot.formacion.AccesingDataWithMySQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesingDataWithMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesingDataWithMySqlApplication.class, args);
+	}
+
+}
